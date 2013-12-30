6f70e17ad0726b60f50538d98d4888427f549cad
scottmotte <scott@scottmotte.com>
2013-12-29 21:42:34 -0800

    Run tests against a configurable string json file

--- test/Smtpapi/Header.php
+++ test/Smtpapi/Header.php
@@ -1,44 +1,54 @@
 <?php 
 
+
 class SmtpapiTest_Header extends PHPUnit_Framework_TestCase {
   
+  protected $t;
+
+  protected function setUp() {
+    $string   = file_get_contents(dirname(__FILE__) . "/../smtpapi_test_strings.json");
+    $json     = json_decode($string, true);
+    $this->t  = $json;
+  }
+
   public function testConstructionHeader() {
     $header = new Smtpapi\Header();
+
     $this->assertEquals(get_class($header), "Smtpapi\Header");
   }
 
   public function testHasAToJsonStringMethod() {
     $header = new Smtpapi\Header();
 
-    $this->assertEquals('{}', $header->toJsonString());
+    $this->assertEquals($this->t['to_json_string'], $header->toJsonString());
   }
 
   public function testAddTo() {
     $header = new Smtpapi\Header();
 
     $header->addTo('addTo@mailinator.com');
-    $this->assertEquals('{"to":["addTo@mailinator.com"]}', $header->toJsonString());
+    $this->assertEquals($this->t['add_to'], $header->toJsonString());
   }
 
   public function testSetTos() {
     $header = new Smtpapi\Header();
 
     $header->setTos(array('addTo@mailinator.com'));
-    $this->assertEquals('{"to":["addTo@mailinator.com"]}', $header->toJsonString());
+    $this->assertEquals($this->t['set_tos'], $header->toJsonString());
   }
 
   public function testAddSubstitution() {
     $header = new Smtpapi\Header();
 
     $header->addSubstitution('sub', array('val'));
-    $this->assertEquals('{"sub":{"sub":["val"]}}', $header->toJsonString());
+    $this->assertEquals($this->t['add_substitution'], $header->toJsonString());
   }
 
   public function testSetSubstitutions() {
     $header = new Smtpapi\Header();
 
     $header->setSubstitutions(array('sub' => array('val')));
-    $this->assertEquals('{"sub":{"sub":["val"]}}', $header->toJsonString());
+    $this->assertEquals($this->t['set_substitutions'], $header->toJsonString());
   }
 
   public function testAddUniqueArg() {
@@ -46,14 +56,14 @@ class SmtpapiTest_Header extends PHPUnit_Framework_TestCase {
 
     $header->addUniqueArg('add_unique_argument_key', 'add_unique_argument_value');
     $header->addUniqueArg('add_unique_argument_key_2', 'add_unique_argument_value_2');
-    $this->assertEquals('{"unique_args":{"add_unique_argument_key":"add_unique_argument_value","add_unique_argument_key_2":"add_unique_argument_value_2"}}', $header->toJsonString());
+    $this->assertEquals($this->t['add_unique_arg'], $header->toJsonString());
   }
 
   public function testSetUniqueArgs() {
     $header = new Smtpapi\Header();
 
     $header->setUniqueArgs(array('set_unique_argument_key' => 'set_unique_argument_value'));
-    $this->assertEquals('{"unique_args":{"set_unique_argument_key":"set_unique_argument_value"}}', $header->toJsonString());
+    $this->assertEquals($this->t['set_unique_args'], $header->toJsonString());
   }
 
   public function testAddCategory() {
@@ -61,14 +71,14 @@ class SmtpapiTest_Header extends PHPUnit_Framework_TestCase {
 
     $header->addCategory('addCategory');
     $header->addCategory('addCategory2');
-    $this->assertEquals('{"category":["addCategory","addCategory2"]}', $header->toJsonString());
+    $this->assertEquals($this->t['add_category'], $header->toJsonString());
   }
 
   public function testSetCategories() {
     $header = new Smtpapi\Header();
 
-    $header->setCategories(array('setCategory'));
-    $this->assertEquals('{"category":["setCategory"]}', $header->toJsonString());
+    $header->setCategories(array('setCategories'));
+    $this->assertEquals($this->t['set_categories'], $header->toJsonString());
   }
 
   public function testAddSection() {
@@ -76,21 +86,21 @@ class SmtpapiTest_Header extends PHPUnit_Framework_TestCase {
 
     $header->addSection('set_section_key', 'set_section_value');
     $header->addSection('set_section_key_2', 'set_section_value_2');
-    $this->assertEquals('{"section":{"set_section_key":"set_section_value","set_section_key_2":"set_section_value_2"}}', $header->toJsonString());
+    $this->assertEquals($this->t['add_section'], $header->toJsonString());
   }
 
   public function testSetSections() {
     $header = new Smtpapi\Header();
 
     $header->setSections(array('set_section_key' => 'set_section_value'));
-    $this->assertEquals('{"section":{"set_section_key":"set_section_value"}}', $header->toJsonString());
+    $this->assertEquals($this->t['set_sections'], $header->toJsonString());
   }
 
   public function testAddFilter() {
     $header = new Smtpapi\Header();
 
     $header->addFilter('footer', 'text/html', '<strong>boo</strong>');
-    $this->assertEquals('{"filters":{"footer":{"settings":{"text/html":"<strong>boo</strong>"}}}}', $header->toJsonString());
+    $this->assertEquals($this->t['add_filter'], $header->toJsonString());
   }
 
 
@@ -107,7 +117,7 @@ class SmtpapiTest_Header extends PHPUnit_Framework_TestCase {
     ); 
 
     $header->setFilters($filter);
-    $this->assertEquals('{"filters":{"footer":{"setting":{"enable":1,"text/plain":"You can haz footers!"}}}}', $header->toJsonString());
+    $this->assertEquals($this->t['set_filters'], $header->toJsonString());
   }
 
 }
