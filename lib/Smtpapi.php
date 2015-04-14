05ccf57bad5d1524e479b6dc0a2dfade88b3fbe5
Eddie Zaneski <eddiezane@gmail.com>
2015-04-13 22:37:45 -0400

    Version bump v0.5.0

--- lib/Smtpapi.php
+++ lib/Smtpapi.php
@@ -2,7 +2,7 @@
 
 class Smtpapi
 {
-    const VERSION = '0.4.0';
+    const VERSION = '0.5.0';
 
     public static function register_autoloader()
     {
--- test/Smtpapi.php
+++ test/Smtpapi.php
@@ -3,7 +3,7 @@ class SmtpapiTest_Smtpapi extends \PHPUnit_Framework_TestCase
 {
     public function testVersion()
     {
-        $this->assertEquals(Smtpapi::VERSION, '0.4.0');
+        $this->assertEquals(Smtpapi::VERSION, '0.5.0');
         $this->assertEquals(
             json_decode(file_get_contents('../composer.json'))->version,
             Smtpapi::VERSION
