b03bb8b3143da031a1d85efe1f3af17273337ee8
joeyz <joeyz5150@gmail.com>
2017-10-04 13:31:44 -0400

    Added filter for $email
    Added regex to sanitize the email input.

--- lib/Smtpapi/Header.php
+++ lib/Smtpapi/Header.php
@@ -24,7 +24,8 @@ class Header
      */
     public function addTo($email, $name = null)
     {
-        $this->to[] = $name ? sprintf('%s <%s>', $name, $email) : $email;
+        $email_sanitized = preg_match('|^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)|i', $email);
+        $this->to[] = $name ? sprintf('%s <%s>', $name, $email_sanitized) : $email_sanitized;
 
         return $this;
     }
