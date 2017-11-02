5cc845cc8b9100ab7771627c8407ade20c36b9c3
yudhik11 <yudhik100@gmail.com>
2017-11-02 16:20:19 +0530

    updated test for license year

--- test/license_test.php
+++ test/license_test.php
@@ -3,9 +3,15 @@ class SmtpapiTest_Smtpapi extends \PHPUnit_Framework_TestCase
 {
     public function testVersion()
     {
-        $f=fopen("./../LICENSE.txt",'r');
-        $line = fgets($f);
-        $this->assertEquals($line,"Copyright (c) 2013-2017 SendGrid, Inc.","It is 2017, so this should be:\nCopyright (c) 2013-2017 SendGrid, Inc.");
-        fclose($f);
+        $ff = explode("\n", file_get_contents('../LICENSE.txt'));
+        $line = '';
+        foreach ($ff as $tp){
+            if (strpos($tp, 'Copyright') === 0){
+                $line = $tp;
+                break;
+            }
+        }
+        $copyright = "Copyright (c) 2013-2017 SendGrid, Inc.";
+        $this->assertEquals($line,$copyright,"It is 2017, so this should be:\nCopyright (c) 2013-2017 SendGrid, Inc.");
     }
 }
